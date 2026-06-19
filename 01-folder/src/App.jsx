d399f5579a4725c3be3b9c10e5adabfaa92edd15
Abhay <abhichamola3@gmail.com>
2026-06-19 17:56:58 +0530

    01-folderdone

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+<div>
+      <div id ="dad">
+        <h1 id = "child1">hello my name is abhay</h1>
+        <h2 id ="child2">welcome wadakam
+        </h2>
+      </div>
+      <div id ="chacha">
+
+      </div>
+</div>
+  )
+}
+
+export default App
+
